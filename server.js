d056a595e3c9f80a7252ca2a456ca9b3157f1eee
Lemon <lemonhe@tudelft.nl>
2025-05-06 19:04:10 +0200

    Add communication with model service

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 const app = express();
 
 // Use Angular App
@@ -12,14 +13,29 @@ app.use(express.json());
 
 
 // POST endpoint to receive user input
-app.post('/userInput', (req, res) => {
-  console.log('Received comment:', req.body.comment);
-  res.status(200).json({
-    status: 'success',
-    message: 'Comment received',
-    receivedComment: req.body.comment
+app.post('/userInput', async (req, res) => {
+    const userInput = req.body.text;
+    console.log(userInput.comment);
+    try {
+      // Step 1: Send request to model-service
+      const modelResponse = await axios.post('http://localhost:5050/predict', {
+        text: userInput.comment
+      });
+  
+      // Step 2: Extract the label
+      const predictedNumber = modelResponse.data.prediction;
+      predictedLabel = "Negative";
+      if (predictedNumber == 1) {
+        predictedLabel = "Positive";
+      }
+  
+      // Step 3: Send the label back to the frontend
+      res.json({ label: predictedLabel });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Model service failed' });
+    }
   });
-});
 
 
 // POST endpoint to update modelOutput
