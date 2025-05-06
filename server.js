0b9ec4b69d581279fe1f722c598461db86f94a71
Lemon <lemonhe@tudelft.nl>
2025-05-06 19:26:45 +0200

    Add model version & clean up code

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const cors = require('cors');
 const axios = require('axios');
 const app = express();
 
+const FRONTEND_PORT = process.env.FRONTEND_PORT;
+const SERVICE_PORT = process.env.SERVICE_PORT;
+const MODEL_PORT = process.env.MODEL_PORT;
+const DNS = process.env.DNS;
+
 // Use Angular App
 app.use(cors({
-    origin: 'http://localhost:4200',
+    origin: `http://${DNS}:${FRONTEND_PORT}`,
     methods: ['GET', 'POST']
 }));
 app.use(express.json());
@@ -15,10 +20,9 @@ app.use(express.json());
 // POST endpoint to receive user input
 app.post('/userInput', async (req, res) => {
     const userInput = req.body.text;
-    console.log(userInput.comment);
     try {
       // Step 1: Send request to model-service
-      const modelResponse = await axios.post('http://localhost:5050/predict', {
+      const modelResponse = await axios.post(`http://${DNS}:${MODEL_PORT}/predict`, {
         text: userInput.comment
       });
   
@@ -29,48 +33,18 @@ app.post('/userInput', async (req, res) => {
         predictedLabel = "Positive";
       }
   
+      model_version = modelResponse.data.version;
       // Step 3: Send the label back to the frontend
-      res.json({ label: predictedLabel });
+      res.json(
+        { 
+            label: predictedLabel, 
+            model_version: model_version
+        });
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Model service failed' });
     }
   });
-
-
-// POST endpoint to update modelOutput
-app.post('/updateModelOutput', (req, res) => {
-    const { analysis, app_version, model_version } = req.body;
-
-    // Update the modelOutput object
-    modelOutput.analysis = analysis || modelOutput.analysis;
-    modelOutput.app_version = app_version || modelOutput.app_version;
-    modelOutput.model_version = model_version || modelOutput.model_version;
-
-    console.log('Updated modelOutput:', modelOutput);
-
-    res.status(200).json({
-        status: 'success',
-        message: 'Model output updated successfully',
-        updatedModelOutput: modelOutput.analysis
-    });
-});
-
-
-// GET endpoint to display on the front end
-let modelOutput = {
-    analysis: "neutral",
-    app_version: "1.2.3",
-    model_version: "1.0.0",
-};
-app.get('/modelOutput', (req, res) => {
-    try {
-        res.status(200).json(modelOutput);
-    } catch (error) {
-        console.error('Error fetching model output:', error);
-        res.status(500).json({ error: 'Failed to retrieve model output' });
-    }
-});
 
 
 //POST endpoint to receive user feedback
@@ -92,7 +66,6 @@ app.post("/judgment", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(SERVICE_PORT, () => {
+  console.log(`Server running on http://localhost:${SERVICE_PORT}`);
 });
